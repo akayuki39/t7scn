ac94d0787e7becfd4541155ca75bf03aba523604
akayuki39 <593740901@qq.com>
2018-07-05 21:58:11 +0800

    change home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   
   def index
-    
+    @categorys = Category.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.delete_all
+Article.delete_all
+
+Category.create!([{name: 'characters'}, {name: 'songs'}])
+Article.create!([
+  {
+    title:    'Kasukebe Haru',
+    category_id: Category.find_by(name: "characters").id,
+    content:
+      %{<p>
+        This is the page for Kasukabe Haru.
+        </p>}
+  },
+  {
+    title:    'Tendouji Musubi',
+    category_id: Category.find_by(name: "characters").id,
+    content:
+      %{<p>
+        This is the page for Tendouji Musubi.
+        </p>}
+  },
+  {
+    title:    'Star☆Glitter',
+    category_id: Category.find_by(name: "songs").id,
+    content:
+      %{<p>
+        This is the page for Star☆Glitter.
+        </p>}
+  } ])
