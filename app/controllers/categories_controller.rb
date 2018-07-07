0a866cbd7025e90403c556a9e919d6f005b4cfa1
akayuki39 <593740901@qq.com>
2018-07-07 19:17:03 +0800

    finish categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,30 @@
 class CategoriesController < ApplicationController
-  def index
-  end
 
   def show
+    @category = Category.find_by(id: params[:id])
+    @articles = @category.articles
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
