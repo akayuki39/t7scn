87af69f23e957c69b72247408c1b2509786981cb
akayuki39 <593740901@qq.com>
2018-07-12 21:31:57 +0800

    add simditor

---
+++ app/assets/javascripts/articles.js
@@ -0,0 +1,15 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+//= require simditor/module
+//= require simditor/uploader
+//= require simditor/hotkeys
+//= require simditor/simditor
+
+// var editor = new Simditor({
+//   textarea: $('#article_content'),
+//   placeholder: 'enter text here...',
+//   toolbar: ['title', 'bold', 'italic', 'underline', 'strikethrough', 'fontScale', 'color', '|', 'ol', 'ul', '|', 'blockquote', 'code', 'table', 'link', 'image', 'hr', 'indent', 'outdent', 'alignment']
+//   //optional options
+// });
