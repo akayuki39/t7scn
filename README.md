762263f082c91def8130c57103f8b495a13f33fb
akayuki39 <593740901@qq.com>
2018-08-12 11:05:22 +0800

    Add update comment to histories

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 
 A Chinese Tokyo 7th sisters wiki(or will be). 
 
+(Demo)[https://t7scn.herokuapp.com/]
+
 ## TODO
 
 1. 历史功能 <- now!
-  ~~将最新版本的文章history id存在Article表中，这样可以加快速度，节省后台计算成本~~
-  ~~创建历史记录页面~~
-  添加每个历史的更新日志
+	~~将最新版本的文章history id存在Article表中，这样可以加快速度，节省后台计算成本~~
+	~~创建历史记录页面~~
+	~~添加每个历史的更新日志comment~~
 1. 添加测试
 1. 文章中右上角提供一个上传介绍图片的地方
 1. 文章页介绍图片左侧位置添加目录，自动根据标题的层级生成
 1. 用户系统
+1. 调整table th td的样式
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180810155807) do
+ActiveRecord::Schema.define(version: 20180812024639) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20180810155807) do
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "comment"
   end
 
 end
