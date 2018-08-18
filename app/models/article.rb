ee443a5bb284c846fb02d526d8c88929125ca153
akayuki39 <593740901@qq.com>
2018-08-18 10:58:39 +0800

    optimistic lock(not finish)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  attr_accessor :default_content#, :original_history_version_id
+  attr_accessor :default_content#, :original_updated_at
   belongs_to :category
   has_many   :histories, dependent: :destroy
 
@@ -25,15 +25,16 @@ class Article < ApplicationRecord
   # end
 
   # # 发生改变的是history 所以应该在history上加乐观锁
-  # def original_history_version_id
-  #   # @original_history_version_id ||= latest_version_id
-  #   @original_history_version_id = latest_version_id
+  # def original_updated_at
+  #   @original_updated_at ||= updated_at.to_f
   # end
 
   # def handle_conflict
-  #   if latest_version_id > @original_history_version_id.to_i
+  #   # updated_at的时间并不是这次更新的时间，而是数据库中的update_at值。
+  #   # 此时还没有update成功，所以这个判断是无效的
+  #   if updated_at.to_f > original_updated_at
   #     @conflict = true
-  #     @original_history_version_id = nil
+  #     @original_updated_at = nil
   #     self.errors.add(:base, "该文章在编辑时已被其他用户修改，请保存您的修改再次尝试")
   #   end
   # end
