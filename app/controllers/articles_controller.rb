e6bed69e610487419ee0e5b16c8f648ba424ecab
akayuki39 <593740901@qq.com>
2018-08-31 19:27:35 +0800

    add authorization check before creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article_and_category, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
 
   # GET /articles
   # GET /articles.json
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def show
     @category = Category.find_by(id: params[:id])
